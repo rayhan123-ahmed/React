7600f23c80dc44904548e24ec1ad9b3e05cbf7dd
Rayhan Ahmed <rmd09181@gmail.com>
2026-05-04 14:44:30 +0600

    feat: refactor App component and add Welcome and Button components

--- core-concepts/src/App.jsx
+++ core-concepts/src/App.jsx
@@ -1,16 +1,12 @@
 
 import './App.css'
- 
+import {Welcome} from './welcome.jsx';
+import {Button} from './button.jsx';
 
-function Welcome() {
-  return <h2>welcome mobin</h2>
-}
 
-function Button () {
-   return <button>Click me </button>
-}
 
-function App() {
+
+export const App = () => {
   return (
     <div>
       <h1>React course</h1>
---
+++ core-concepts/src/welcome.jsx
@@ -0,0 +1,4 @@
+ export const  Welcome = () => {
+  return <h2>welcome mobin</h2>;
+}
+
