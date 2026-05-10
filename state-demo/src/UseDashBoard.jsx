627667d315b8c05127e47ab78acb02409ecebe47
Rayhan Ahmed <rmd09181@gmail.com>
2026-05-10 22:18:25 +0600

    fix: correct conditional rendering and formatting in UserDashBoard component

--- state-demo/src/UseDashBoard.jsx
+++ state-demo/src/UseDashBoard.jsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 
 export const UserDashBoard = ({ isPremium }) => {
   const [Cradits, setCredits] = useState(100);
-  if (!isPremium) {
+  if (isPremium) {
     return <p>Upgrade to premium to see the credits</p>;
   }
   return (
     <div>
-      <p>You have{Cradits} Credits</p>
+      <p>You have {Cradits} Credits</p>
       <button
         onClick={() => {
           setCredits(0);
         }}
-      ></button>
+      >
+        Reset Credits
+      </button>
     </div>
   );
 };
