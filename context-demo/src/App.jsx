27d61368b8304e0a3b48b147ff3d4e58119788dc
Rayhan Ahmed <rmd09181@gmail.com>
2026-07-06 00:48:05 +0600

    feat: add user context and components for dashboard

---
+++ context-demo/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Header } from "./Header";
+import { UserContext } from "./UserContext";
+
+function App(){
+  const user = {
+    name : 'bruce',
+    role : 'Admin',
+    theme : 'dark'
+  }
+  return (
+    <UserContext value={user}>
+      <div>
+        <h1>Dashboard</h1>
+        <Header />
+      </div>
+    </UserContext>
+  );
+}
+
+
+
+export default App
